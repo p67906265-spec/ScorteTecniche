73fe7a30031aea5d10e79b2f9d5014e6c25a3fc5
Paolo <p67906265-spec@users.noreply.github.com>
2026-08-27 07:59:35 +0200

    Scorte Tecniche v1.0 clean

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scorte Tecniche
+
+Android app per il sito https://scortetecniche.rf.gd
+
+Versione iniziale: 1.0
+Application ID: it.paolo.scortetecniche
+
+Il workflow GitHub Actions genera l'APK release come artifact:
+ScorteTecniche-v1.0-unsigned
+
+La firma permanente verrà aggiunta usando GitHub Secrets.
---
+++ app/src/main/java/it/paolo/scortetecniche/MainActivity.java
@@ -0,0 +1,46 @@
+package it.paolo.scortetecniche;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String HOME = "https://scortetecniche.rf.gd";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setSupportZoom(false);
+        s.setBuiltInZoomControls(false);
+
+        CookieManager cm = CookieManager.getInstance();
+        cm.setAcceptCookie(true);
+        cm.setAcceptThirdPartyCookies(webView, true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl(HOME);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
